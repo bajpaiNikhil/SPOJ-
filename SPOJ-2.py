a4225eef97f946a0665d28e3b845297bdb02bf1e
Nikhil's Empire <nikhil.cse16@gmail.com>
2018-11-08 09:57:13 +0530

    Spoj Quest to 100!

---
+++ SPOJ-2.py
@@ -0,0 +1,12 @@
+"""t=int(input())
+for t in range(t):
+    n,m=map(int,input().split())
+    c=str(n)[::-1]
+    z=int(c)
+    #print(z)
+    d=str(m)[::-1]
+    y=int(d)
+    e=z+y
+    f=str(e)[::-1]
+    g=int(f)
+    print(g)"""
---
+++ SPOJ-3.py
@@ -0,0 +1,11 @@
+"""import math
+def countRect(n):
+    ans = 0
+    for length in range(1, int(math.sqrt(n)) + 1):
+        height = length
+        while (height * length <= n):
+            ans += 1
+            height += 1
+    return ans
+n = int(input())
+print(countRect(n))"""
